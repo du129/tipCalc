ec86eff7f5a95932d63d9526277b20ba2ff0e02e
Jonathan Du <jonathandu@pal-nat186-153-75.itap.purdue.edu>
2018-01-11 20:49:21 -0500

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Jonathan Du on 1/11/18.
+//  Copyright © 2018 Jonathan Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
